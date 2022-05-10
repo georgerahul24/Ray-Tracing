2a439a6c253e61c9fd6e12d3e6225dd322a81c87
George Rahul <75750164+georgerahul24@users.noreply.github.com>
2022-05-10 23:29:33 +0530

    adding vectors and ray

--- Ray Tracing/Program.cs
+++ Ray Tracing/Program.cs
@@ -1,17 +1,10 @@
 ﻿using Ray_Tracing;
-/*
+
 string file = @"image.ppm";
-const int image_height = 1920, image_width = 1920, color_depth = 1920;
-try
-{
-    File.Delete(file);
-}
-catch
-{
-    
-}
-using StreamWriter w = File.AppendText("image.ppm");
-w.Write($"P3\n{image_height} {image_width}\n{color_depth}\n");
+const int image_height = 256, image_width = 256;
+File.Delete(file);
+Renderer w = new(file,image_height,image_height,255.99);
+
 
 for (int j = image_height - 1; j >= 0; j--)
 {
@@ -20,20 +13,21 @@ for (int j = image_height - 1; j >= 0; j--)
     for (int i = 0; i < image_width; i++)
 
     {
-      
-        int r =(int) (i*color_depth/ (image_width-1)), g = (int)(j*color_depth / (image_height-1)), b=(int)(i+j)/2*color_depth;
-        w.Write($"{r} {g} {b}\n");
+        w.WriteColor((double)i/ (image_height - 1),(double)j/ (image_width - 1), 0.25);
 
 
     }
 }
-*/
+
 Vector a = new(1,2,3);
-Vector b = new(1,2,3);
-Vector c = a + b;
-c += b;
-c = c*4;
+Vector ab = new(1,2,3);
+Vector c = a + ab;
+c += ab;
+c *= 4;
+c = c/4;
+
 Console.Write(c);
+Console.WriteLine(c.Length());
 
 
 
--- Ray Tracing/Tools.cs
+++ Ray Tracing/Tools.cs
@@ -2,13 +2,13 @@
 
 public class Vector
 {
-    private int[] Point;
-    private int _x, _y, _z;
-    public Vector(int x,int y,int z)
+    private double[] Point;
+    public double X, Y, Z;
+    public Vector(double x,double y,double z)
     {
-        _x = x;
-        _y = y;
-        _z = z;
+        X = x;
+        Y = y;
+        Z = z;
         Point = new[]{x,y,z};
         
     }
@@ -25,19 +25,83 @@ public class Vector
         return res;
     }
 
-    public static Vector operator *(Vector a,int t)
+    public static Vector operator *(Vector a,double t)
     {
         Vector res = new Vector(a.Point[0]*t, a.Point[1] *t, a.Point[2]*t);
         return res;
     }
- 
-    
-   
-    
+    public static Vector operator /(Vector a,double t)
+    {
+        Vector res = new(a.X/t,a.Y/t,a.Z/t);
+        return res;
+    }
 
+    public double Length()
+    {
+        return Math.Sqrt(Math.Pow(X,2)+Math.Pow(Y,2)+Math.Pow(Z,2));
+    }
+
+    
     public override string ToString()
     {
-        return $"{_x}i + {_y}j + {_z}k";
+        return $"{X}i + {Y}j + {Z}k";
     }
     
+}
+
+public class Renderer
+{
+    private StreamWriter _w;
+    private string _filename;
+    public double ColorDepth;
+    public int ImageHeight;
+    public int ImageWidth;
+    public Renderer(string filename,int image_height = 256,int image_width = 256,double color_depth = 255.99)
+    {
+        _filename = filename;
+        ColorDepth = color_depth;
+        ImageHeight = image_height;
+        ImageWidth = image_width;
+
+        StreamWriter w = File.AppendText(filename);
+        _w = w;
+        w.WriteLine($"P3\n{image_height} {image_width}\n{(int)color_depth}");
+    }
+
+    public void Delete()
+    {
+        try
+        {
+            File.Delete(_filename);
+        }
+        catch
+        {
+            // ignored
+        }
+    }
+
+    public void Write(string s)
+    {
+        _w.WriteLine(s);
+    }
+    public void WriteColor(double r,double g, double b)
+    {
+        _w.WriteLine($"{(int)(r * ColorDepth)} {(int)(g*ColorDepth)} {(int)(b * ColorDepth)}");
+    }
+}
+
+public class Ray
+//Ray => A +tB where A is orgin, t is a variable and B is the direction of the vector
+{
+    private Vector org, dir;
+    public Ray(Vector org, Vector dir)
+    {
+        this.org = org;
+        this.dir = dir;
+    }
+
+    public Vector at(double t)
+    {
+        return org +  dir*t;
+    }
 }
