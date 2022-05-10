82492ff2ccb1fe3438afcc89b6460dea40f9d90e
George Rahul <75750164+georgerahul24@users.noreply.github.com>
2022-05-10 22:35:40 +0530

    initial commit
    - Designing Vector System

---
+++ Ray Tracing/Program.cs
@@ -0,0 +1,41 @@
+﻿using Ray_Tracing;
+/*
+string file = @"image.ppm";
+const int image_height = 1920, image_width = 1920, color_depth = 1920;
+try
+{
+    File.Delete(file);
+}
+catch
+{
+    
+}
+using StreamWriter w = File.AppendText("image.ppm");
+w.Write($"P3\n{image_height} {image_width}\n{color_depth}\n");
+
+for (int j = image_height - 1; j >= 0; j--)
+{
+    Console.SetCursorPosition(0,0);
+    Console.WriteLine($"Remaining {j}");
+    for (int i = 0; i < image_width; i++)
+
+    {
+      
+        int r =(int) (i*color_depth/ (image_width-1)), g = (int)(j*color_depth / (image_height-1)), b=(int)(i+j)/2*color_depth;
+        w.Write($"{r} {g} {b}\n");
+
+
+    }
+}
+*/
+Vector a = new(1,2,3);
+Vector b = new(1,2,3);
+Vector c = a + b;
+c += b;
+c = c*4;
+Console.Write(c);
+
+
+
+
+
---
+++ Ray Tracing/Tools.cs
@@ -0,0 +1,43 @@
+﻿namespace Ray_Tracing;
+
+public class Vector
+{
+    private int[] Point;
+    private int _x, _y, _z;
+    public Vector(int x,int y,int z)
+    {
+        _x = x;
+        _y = y;
+        _z = z;
+        Point = new[]{x,y,z};
+        
+    }
+
+    public static Vector  operator + (Vector a, Vector b)
+    {
+        Vector res = new Vector(a.Point[0] + b.Point[0], a.Point[1] + b.Point[1], a.Point[2] + b.Point[2]);
+        return res;
+    }
+
+    public static Vector operator -(Vector a, Vector b)
+    {
+        Vector res = new Vector(a.Point[0] - b.Point[0], a.Point[1] - b.Point[1], a.Point[2] - b.Point[2]);
+        return res;
+    }
+
+    public static Vector operator *(Vector a,int t)
+    {
+        Vector res = new Vector(a.Point[0]*t, a.Point[1] *t, a.Point[2]*t);
+        return res;
+    }
+ 
+    
+   
+    
+
+    public override string ToString()
+    {
+        return $"{_x}i + {_y}j + {_z}k";
+    }
+    
+}
