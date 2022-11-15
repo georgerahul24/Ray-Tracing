cd66fad057d4faf8c91da16b5894acfe04acef15
George Rahul <75750164+georgerahul24@users.noreply.github.com>
2022-11-15 23:28:44 +0530

    Added Vec3 file's first part
    - Also added compiler flag to use c++ version 17
    
    - Moving the compiled files to `bin` folder

--- Ray Tracing in C++/Renderer.h
+++ Ray Tracing in C++/Renderer.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "FileWriter.h"
+
 class Renderer{
 
 private:
@@ -7,17 +8,18 @@ private:
 	 double colormult;
 	 FileWriter file_writer;
 public:
-	Renderer(int image_width,int image_height):file_writer("image.ppm"){
-		Renderer::image_width=image_width;
-		Renderer::image_height=image_height;
+	Renderer(int* image_width,int* image_height):file_writer("image.ppm"){
+		
+		Renderer::image_width = *image_width;
+		Renderer::image_height = *image_height;
 		colormult=255.99;
-		file_writer.file << "P3\n" << image_width<<" "<<image_height<<"\n256\n"; //Writing the image types
+		file_writer.file << "P3\n" << *image_width<<" "<<*image_height<<"\n256\n"; //Writing the image types
 	}
 	
 	void start(){
-			for (int j=image_height-1;j>=0;--j){
+			for (int j=image_height-1;j>=0;j--){
 		      std::cerr << "Progress: " <<j+1<< "/"<<image_height; // Progress Bar Header
-		      for (int i=image_width-1;i>=0;--i){
+		      for (int i=0; i<image_width; i++){
 		        double r=double(i)/(image_width-1);
 		        double g=double(j)/(image_height-1);
 		        double b=0.25;
--- Ray Tracing in C++/main.cpp
+++ Ray Tracing in C++/main.cpp
@@ -1,14 +1,23 @@
-#include <iostream>
-#include "Renderer.h"
-int main(){
-  const int image_width=256;
-  const int image_height=256;
 
- 
-  Renderer renderer(image_width,image_height);
+#include "Renderer.h"
+#include "Vec3.h"
+
+
+int main(){
+  int* image_width= new int;
+  int* image_height=new int;
+  *image_width=256;
+  *image_height=256;
   
+
+ if (__cplusplus!=201703)
+  {std::cout<< "C++ Version used is not C++17 and thus might cause some errors due to the same"<<std::endl;}
+  
+  Renderer renderer(image_width,image_height);
  
   renderer.start();
+  delete image_width;
+  delete image_height;
   
   
   
